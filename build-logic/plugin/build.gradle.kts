e7a7971409a78294e5f974a044eaaabaed6c06da
lyh990517 <lyh990517@naver.com>
2025-02-14 10:20:01 +0900

    Initial commit

---
+++ build-logic/plugin/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    `kotlin-dsl`
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+dependencies {
+    compileOnly(libs.android.tools.build.gradle)
+    compileOnly(libs.kotlin.gradle.plugin)
+}
